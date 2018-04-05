7594a96097b2155d01aaf99abc058c937060f34b
sbb <jiaxian>
2018-04-05 23:46:52 +0800

    add:新增下载管理器

--- main.go
+++ main.go
@@ -5,7 +5,16 @@ import (
 )
 
 func main() {
-	u := &urlManager{}
-	u.addNewUrl("12")
-	fmt.Println("vim-go")
+	u := NewUrlManager()
+	h := NewHtmlDownloader()
+	u.addNewUrl("https://www.baidu.com/")
+	url, err := u.getNewUrl()
+	if err != nil {
+		panic(err)
+	}
+	res, err := h.download(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res)
 }
--- urlManager.go
+++ urlManager.go
@@ -5,15 +5,29 @@ import (
 )
 
 type urlManager struct {
-	NewUrls []string
-	OldUrls []string
+	NewUrls map[int]string
+	OldUrls map[int]string
+	newIndex int
+	oldIndex int
+}
+func NewUrlManager() *urlManager {
+	u := new(urlManager)
+	u.NewUrls = make(map[int]string)
+	u.OldUrls = make(map[int]string)
+	
+	u.newIndex = 0
+	u.oldIndex = 0
+
+	return u
+    
 }
 
 func (u *urlManager) addNewUrl(url string) error {
 	if url == "" {
 		return fmt.Errorf("%s", "error")
 	}
-	u.NewUrls = append(u.NewUrls, url)
+	u.NewUrls[u.newIndex] = url
+	u.newIndex++;
 	return nil
 }
 
@@ -27,6 +41,18 @@ func (u *urlManager) addNewUrls(urls []string) error {
 	return nil
 }
 
-func (u *urlManager) print() {
-	fmt.Println(u.NewUrls)
+
+func (u *urlManager) hasNewUrl() bool {
+	return len(u.NewUrls) != 0
 }
+
+func (u *urlManager) getNewUrl() (string, error){
+	if !u.hasNewUrl() {
+		return "", fmt.Errorf("%s", "didn't have index")
+	}
+	url := u.NewUrls[u.oldIndex]
+	u.OldUrls[u.oldIndex] = url
+	u.oldIndex++;
+	return url, nil;
+}
+
